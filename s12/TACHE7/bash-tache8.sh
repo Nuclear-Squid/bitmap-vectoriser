085bed1fc813f644af409456ddfb2b80974adec6
Ecrom <samuelepin@yahoo.com>
2023-04-25 15:22:29 +0200

    ajou du bash pour la tache 8

---
+++ s12/TACHE7/bash-tache8.sh
@@ -0,0 +1,10 @@
+for fich in {"Asterix3","lettre-L-cursive","ColombesDeLaPaix"}; do
+for dis in {"0","0,5","1","2","4","8","16"}; do 
+for type in {"segments","bezier2","bezier3"}; do 
+echo Le nb contours pour $fich.pbm :
+./bitmap-vectoriser -i image_test/$fich.pbm -o sortie_eps/$fich-d$dis-t$type.eps -d $dis -s fill -m $type
+echo  " "
+done
+done
+done
+
